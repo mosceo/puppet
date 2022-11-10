f1581ed6581ea9f133954a6ac88e71b09158af63
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-10 16:27:52 +1000

    add

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,4 +1,5 @@
 cron { 'cron example':
+  ensure => absent,
   command => 'echo "killing..." >> /tmp/debug.log',
   user    => 'roman',
   # environment => ['MAILTO=admin@example.com', 'PATH=/bin'],
