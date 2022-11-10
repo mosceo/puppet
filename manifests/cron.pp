aaa545eab21b92166a505890ce33774c6bcb6df8
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-10 16:04:35 +1000

    add

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -5,3 +5,11 @@ cron { 'cron example':
   hour        => '0',
   minute      => '0',
 }
+
+cron { 'cron example 2':
+  command     => 'touch /tmp/debug-touched.log',
+  user        => 'roman',
+  # environment => ['MAILTO=admin@example.com', 'PATH=/bin'],
+  hour        => '0',
+  minute      => '0',
+}
