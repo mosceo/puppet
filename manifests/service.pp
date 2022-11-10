b56e62ecde3490ca5b4f607d0ad89f0a2c9427c9
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-10 14:29:36 +1000

    add

--- manifests/service.pp
+++ manifests/service.pp
@@ -13,7 +13,7 @@ file { '/tmp/config':
 file { '/tmp/config/nginx.conf':
   ensure => present,
   notify => Service['nginx'],
-  content => 'apple pie',
+  content => 'apple pie 2\n',
 }
 
 service { 'nginx':
