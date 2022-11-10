926a4d37adb964a69507aa5b6464580f8a062c5b
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-10 15:01:20 +1000

    add

--- manifests/user.pp
+++ manifests/user.pp
@@ -17,3 +17,9 @@ user { 'grupman':
   home   => '/tmp',
   shell  => '/usr/sbin/nologin',
 }
+
+ssh_authorized_key { 'roman':
+  user => 'roman',
+  type => 'ssh-rsa',
+  key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABgQClrU62f+BVVrwlNiXsSLsByl5lgthb3Wer2xT2WLCIftmoxep09vGpnVvtdQHORPAOF/z5kau0N6rOja1ievc/LLPmymdat6kRxaQ3E1YHtQWc6+al9XQsPhICfDteFEO91d4dyJm8EinWe8JZs9MhgBEteQq4oE9RcRIKrflheBT1XjVjbybRfT4hmEMna9+zjf5+EsfBGg6zCeI+zElkOjpSKhfJYAYevcjgD4PAvfz3BtOxt7aO8rct0AiK8NjLQwCQ8emah64YV6PeuNmolXu5xF478qFAe+hhcrMU1Pj2UP2XzKoAvPhcU8wVLKWl+Fi8CBGciekVzx5TEveSM4eN5IFYBihrFFnUZWZCE+EL6PLRkmOe8tOhG9qX0eGnhgrgZfsZz5m9IDgSNmNpBV2u8aEkQjiJtrMt1aYOMZuSXiFkl1sQiV7ixMV80Wp0IN+j4dwxMnZkhQRyx8544v6fimpWBDwZk51VjVCN4xN3EZpE2fHLhS0KcQJIUDc= mosceo@puma',
+}
