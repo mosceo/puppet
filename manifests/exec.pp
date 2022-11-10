fd31babbf72e14c92886ce41dfd45721bd5ab42c
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-11 07:38:38 +1000

    add

--- manifests/exec.pp
+++ manifests/exec.pp
@@ -1,5 +1,6 @@
 exec { 'install-cat-picture-generator':
-  ensure => absent,
+  # ensure => absent,
   cwd     => '/tmp',
-  command => '/usr/bin/touch file-touched.log',
+  command => '/usr/bin/echo "write once" >> file-touched.log',
+  onlyif => '/usr/bin/ls file-touched.log',
 }
