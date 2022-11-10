eaa8d2b90da2721c4489e6f33edcca8123c0a37b
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-11 07:48:16 +1000

    add

--- manifests/exec.pp
+++ manifests/exec.pp
@@ -1,6 +1,16 @@
-exec { 'install-cat-picture-generator':
-  # ensure => absent,
-  cwd     => '/tmp',
-  command => '/usr/bin/echo "write once" >> file-touched.log',
-  unless => '/usr/bin/ls file-touched.log',
+# exec { 'install-cat-picture-generator':
+#   # ensure => absent,
+#   cwd     => '/tmp',
+#   command => '/usr/bin/echo "write once" >> file-touched.log',
+#   unless  => '/usr/bin/ls file-touched.log',
+# }
+
+file { '/tmp/aliases':
+  content => 'root: john@bitfieldconsulting.com',
+  notify  => Exec['aliases-changed'],
+}
+
+exec { 'aliases-changed':
+  command     => '/usr/bin/echo "aliases file changed" >> aliases-changes.log',
+  refreshonly => true,
 }
