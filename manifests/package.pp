1fd333c5d0ecfd9b8561f8a69703c9caaaf0bb2d
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-10 14:12:33 +1000

    add

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,11 +1,3 @@
-package { 'nginx':
-  ensure => purged,
-}
-
-package { 'nginx-common':
-  ensure => purged,
-}
-
 package { 'cowsay':
   ensure => '3.03+dfsg2-8',
 }
---
+++ manifests/service.pp
@@ -0,0 +1,4 @@
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
