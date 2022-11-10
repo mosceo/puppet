93d1c03ed180880d8bb58c84e9c6b1d3b4ec4612
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-10 16:12:36 +1000

    add

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,21 +1,21 @@
-cron { 'cronexample':
-  command     => 'echo "work..." >> /tmp/debug.log',
-  user        => 'roman',
-  # environment => ['MAILTO=admin@example.com', 'PATH=/bin'],
-  hour        => '*',
-  minute      => '*',
-}
-
-cron { 'cronexample2':
-  command     => 'touch /tmp/debug-touched.log',
-  user        => 'roman',
-  # environment => ['MAILTO=admin@example.com', 'PATH=/bin'],
-  hour        => '*',
-  minute      => '*',
-}
+# cron { 'cronexample':
+#   command     => 'echo "work..." >> /tmp/debug.log',
+#   user        => 'roman',
+#   # environment => ['MAILTO=admin@example.com', 'PATH=/bin'],
+#   hour        => '*',
+#   minute      => '*',
+# }
+#
+# cron { 'cronexample2':
+#   command     => 'touch /tmp/debug-touched.log',
+#   user        => 'roman',
+#   # environment => ['MAILTO=admin@example.com', 'PATH=/bin'],
+#   hour        => '*',
+#   minute      => '*',
+# }
 
 cron { 'cronexample3':
-  command     => 'ls',
+  command     => '/etc/puppet/code/puppet/files/log.sh',
   user        => 'roman',
   # environment => ['MAILTO=admin@example.com', 'PATH=/bin'],
   hour        => '*',
