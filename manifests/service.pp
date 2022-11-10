f5c1dd2fa3a69c55ccc274458e9461931643cc24
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-10 14:23:55 +1000

    add

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,7 +6,13 @@ package { 'nginx-common':
   ensure => installed,
 }
 
+file { '/tmp/config/nginx.conf':
+  ensure => present,
+  notify => Service['nginx'],
+}
+
 service { 'nginx':
-  ensure => stopped,
-  enable => true,
+  ensure  => stopped,
+  enable  => true,
+  restart => 'echo "restarting NGINX" >> /tmp/debug.log && systemctl restart nginx',
 }
