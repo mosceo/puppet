a44ded151abca113f2f2191b29bfd009b6584e9a
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-12 20:19:41 +1000

    add

--- manifests/facts.pp
+++ manifests/facts.pp
@@ -1,3 +1,7 @@
-notice($facts['os']['release']['major'])
-notice($facts['path'])
-notice($facts['networking']['fqdn'])
+$tasks = ['task1', 'task2', 'task3']
+$tasks.each | $task | {
+  file { "/usr/local/bin/${task}":
+    content => "echo I am ${task}\n",
+    mode    => '0755',
+  }
+}
