57a13ba17b007ed01ee80cd805890918619e31fd
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-12 18:22:47 +1000

    add

--- manifests/variables.pp
+++ manifests/variables.pp
@@ -16,6 +16,22 @@ if $php_present {
   }
 }
 
+$webserver = 'apache'
+case $webserver {
+  'nginx': {
+    notice("Looks like you're using Nginx! Good choice!")
+  }
+  'apache': {
+    notice("Ah, you're an Apache fan, eh?")
+  }
+  'IIS': {
+    notice('Well, somebody has to.')
+  }
+  default: {
+    notice("I'm not sure which webserver you're using!")
+  }
+}
+
 # $php_package = 'php-cli'
 
 # $dependencies = [
