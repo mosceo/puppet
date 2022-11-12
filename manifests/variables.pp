f3d1218163d6d1d313ba8f34c33e43ffb1a05266
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-12 10:19:46 +1000

    add

--- manifests/variables.pp
+++ manifests/variables.pp
@@ -1,9 +1,20 @@
 $php_package = 'php-cli'
 
-package { $php_package:
+$dependencies = [
+  'php8.1-cgi',
+  'php8.1-cli',
+  'php8.1-common',
+  'php8.1-gd',
+  'php8.1-json',
+  'php8.1-mcrypt',
+  'php8.1-mysql',
+  'php8.1-soap',
+]
+
+package { $dependencies:
   ensure => installed,
 }
 
-$admin_names = ['Dave', 'Mike', 'John']
-$admin_name = $admin_names[1]
-notice("Hello, ${admin_name}! It's great to meet you!")
+# $admin_names = ['Dave', 'Mike', 'John']
+# $admin_name = $admin_names[1]
+# notice("Hello, ${admin_name}! It's great to meet you!")
