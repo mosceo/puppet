7875898e0ec09da21e292c1d85c742dc2e8969cd
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-13 09:53:07 +1000

    add

--- manifests/test/facts.pp
+++ manifests/test/facts.pp
@@ -2,3 +2,5 @@ $nics = $facts['networking']['interfaces']
 $nics.each | String $interface, Hash $attributes | {
   notice("Interface ${interface} has IP ${attributes['ip']}")
 }
+
+notice('workes!')
