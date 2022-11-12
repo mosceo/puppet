68bf10a5cd860234a493955b5c4bd6a5845ad644
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-12 20:23:02 +1000

    add

--- manifests/facts.pp
+++ manifests/facts.pp
@@ -1,7 +1,12 @@
-$tasks = ['task1', 'task2', 'task3']
-$tasks.each | $task | {
-  file { "/usr/local/bin/${task}":
-    content => "echo I am ${task}\n",
-    mode    => '0755',
-  }
+# $tasks = ['task1', 'task2', 'task3']
+# $tasks.each | $task | {
+#   file { "/usr/local/bin/${task}":
+#     content => "echo I am ${task}\n",
+#     mode    => '0755',
+#   }
+# }
+
+$nics = $facts['networking']['interfaces']
+$nics.each | String $interface, Hash $attributes | {
+  notice("Interface ${interface} has IP ${attributes['ip']}")
 }
