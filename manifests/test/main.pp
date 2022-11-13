3e4951e50759a893fe055c3445e134bb319d384d
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-13 11:09:30 +1000

    add

--- manifests/test/main.pp
+++ manifests/test/main.pp
@@ -1,14 +1,9 @@
-# $nics = $facts['networking']['interfaces']
-# $nics.each | String $interface, Hash $attributes | {
-#   notice("Interface ${interface} has IP ${attributes['ip']}")
-# }
-
-
 $dir_path = lookup('x_dir')
 notice($dir_path)
 notice("Some text: $dir_path")
 notice("Some text: ${dir_path}")
 notice("Some text: ${lookup('x_dir')}")
+notice("Some text: ", lookup('x_dir'))
 
 file { $dir_path:
   ensure => directory,
