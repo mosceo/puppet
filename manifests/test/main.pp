f26f19acf5a42a662c2632854a52959796518d5a
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-13 11:16:32 +1000

    add

--- manifests/test/main.pp
+++ manifests/test/main.pp
@@ -4,3 +4,7 @@ $cobbler_config = lookup('cobbler_config', Hash)
 if $cobbler_config['manage_dhcp'] {
   notice('DHCP is managed')
 }
+
+if lookup('cobbler_config.manage_dhcp', Boolean) {
+  notice('DHCP is managed v2')
+}
