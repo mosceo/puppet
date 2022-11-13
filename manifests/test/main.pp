fd725d5aef6bb0a516108a6a6c2a972676bfade6
Роман Кунин <kunin.ra@dns-shop.ru>
2022-11-13 11:08:10 +1000

    add

--- manifests/test/main.pp
+++ manifests/test/main.pp
@@ -6,6 +6,9 @@
 
 $dir_path = lookup('x_dir')
 notice($dir_path)
+notice("Some text: $dir_path")
+notice("Some text: ${dir_path}")
+notice("Some text: ${lookup('x_dir')}")
 
 file { $dir_path:
   ensure => directory,
